9539f1624cba7444896732b2a016d2a7deeb1f5e
AntonMisiura <misiura.anton@gmail.com>
2017-04-25 17:46:25 +0300

    initial

---
+++ AsyncSocket/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncSocket
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            AsynchronousSocketListener.StartListening();
+            return 0;
+        }
+    }
+}
